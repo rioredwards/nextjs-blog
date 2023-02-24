80fee03d0d595e7b58b52882e4dc9296804f4f3f
Rio Edwards <rioredwards@gmail.com>
2023-02-23 18:45:50 -0800

    Add <Head> tag and <Date> tag

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,12 @@
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
+import Date from "../../components/date";
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id);
+
   return {
     props: {
       postData,
@@ -21,11 +25,16 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
       {postData.title}
       <br />
       {postData.id}
       <br />
-      {postData.date}
+      <Date dateString={postData.date} />
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
